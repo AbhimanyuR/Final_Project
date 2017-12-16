53b2003501e2d9e3253aaf7c2b0c9cfde0d66209
Pareekshith A Abhimanyu Raja <paa36@afsconnect2.njit.edu>
2017-12-15 23:56:47 -0500

    Added and edited created method

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -49,7 +49,16 @@ class tasksController extends http\controller
 
     public static function create()
     {
-        print_r($_POST);
+        $todo = new todo();
+        session_start();
+        date_default_timezone_set("Asia/Bangkok");
+        $todo->ownerid = $_SESSION ['userID'];
+        $todo->createddate = date("Y/m/d");
+        $todo->isdone = $_POST['isdone'];
+        $todo->message = $_POST['message'];
+        $todo->owneremail = $_POST['owneremail'];
+        $todo->duedate = $_POST['duedate'];
+        $todo->save();
     }
 
     //this is the function to view edit record form
