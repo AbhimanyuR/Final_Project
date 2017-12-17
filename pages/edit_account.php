6c6a6b4eb6236daa5d4bc7185035d38a4bf961ca
Pareekshith A Abhimanyu Raja <paa36@afsconnect2.njit.edu>
2017-12-17 07:16:14 -0500

    edit_account layout updated

--- pages/edit_account.php
+++ pages/edit_account.php
@@ -1,1 +1,151 @@
-put a page with in a form to edit an account
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Register page</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="pages/assets/css/style.css?v=1.0">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Mysite</a>
+                </div>
+        
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li  class="active">
+                            <a href="/">MY account</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=tasks&action=create"  >Add New Task</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=tasks&action=all">Tasks</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=accounts&action=logout">Logout</a>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
+        </nav>
+
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                    
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <div class="edit-profile-form">
+                        <form class="form-horizontal"  action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">First Name:</label>
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="username" value="<?php echo $data->fname; ?>"  name="fname" placeholder="Enter Username">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Last Name:</label>
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="username" value="<?php echo $data->lname; ?>"  name="lname" placeholder="Enter Username">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Email:</label>
+                            <div class="col-sm-10">
+                              <input type="email" class="form-control" id="username"  value="<?php echo $data->email; ?>"  name="email" placeholder="Enter Username">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Phone:</label>
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="username" value="<?php echo $data->phone; ?>" name="phone" placeholder="Enter Username">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Birthday:</label>
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="username" value="<?php echo $data->birthday; ?>"  name="birthday" placeholder="Enter Username">
+                            </div>
+                          </div>
+                          <div class="form-group">
+                            <label class="control-label col-sm-2" for="email">Gender:</label>
+                            <div class="col-sm-10">
+                              <input type="text" class="form-control" id="username" value="<?php echo $data->gender; ?>"  name="gender" placeholder="Enter Username">
+                            </div>
+                          </div>
+                          <div class="form-group"> 
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <div class="checkbox">
+                                <label><input type="checkbox"> Remember me</label>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="form-group"> 
+                            <div class="col-sm-offset-2 col-sm-10">
+                              <button type="submit" class="btn btn-default">Submit</button>
+                            </div>
+                          </div>
+                        </form>
+
+
+                        <form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+                            <button type="submit" form="form1" value="delete">Delete</button>
+                        </form>
+
+                    </div>
+                </div>
+
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    
+                </div>
+            </div>
+        </div>
+
+
+
+        <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+            <div class="container">
+                <a class="navbar-brand" href="#">My footer</a>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- jQuery -->
+        <script src="//code.jquery.com/jquery.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+         <script src="Hello World"></script>
+    </body>
+</html>
