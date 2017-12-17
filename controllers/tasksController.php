5880854833c0c1fd63a3f16d20d453567ce7f75c
Pareekshith A Abhimanyu Raja <paa36@afsconnect2.njit.edu>
2017-12-16 22:40:49 -0500

    Edited functions

--- controllers/tasksController.php
+++ controllers/tasksController.php
@@ -14,60 +14,60 @@ class tasksController extends http\controller
     //to call the show function the url is index.php?page=task&action=show
     public static function show()
     {
-        $todo = todos::findOne($_REQUEST['id']);
-        self::getTemplate('todo_edit', $todo);
+        $record = todos::findOne($_REQUEST['id']);
+        self::getTemplate('show_task', $record);
     }
 
-    public static function newTodoform()
-    {
-        $todo = new todo();
-        self::getTemplate('todo', $todo);
-    }
     //to call the show function the url is index.php?page=task&action=list_task
 
     public static function all()
     {
-        $records = todos::findAll();
-        /*session_start();
-           if(key_exists('userID',$_SESSION)) {
-               $userID = $_SESSION['userID'];
-           } else {
+        // $records = todos::findAll();
+        session_start();
+        if(key_exists('userID',$_SESSION)) {
+            $userID = $_SESSION['userID'];
+            $userID = $_SESSION['userID'];
+            $userData = accounts::findOne($userID);
+            $records = todos::findTasksbyID($userID);
+            if (is_array($records)) {
+                foreach ($records as $eachTask) {
+                    if ($eachTask->isdone =='1') {
+                        $eachTask->isdone = 'Yes';
+                    }elseif ($eachTask->isdone=='0') {
+                        $eachTask->isdone = 'No';
+                    }
 
-               echo 'you must be logged in to view tasks';
-           }
-        $userID = $_SESSION['userID'];
+                }
+            }
+            self::getTemplate('all_tasks', $records);
+        } else {
 
-        $records = todos::findTasksbyID($userID);
-        */
-        self::getTemplate('all_tasks', $records);
+            echo 'you must be logged in to view tasks';
+        }
 
     }
     //to call the show function the url is called with a post to: index.php?page=task&action=create
     //this is a function to create new tasks
 
     //you should check the notes on the project posted in moodle for how to use active record here
-
+//
     public static function create()
     {
-        $todo = new todo();
+
         session_start();
-        date_default_timezone_set("Asia/Bangkok");
-        $todo->ownerid = $_SESSION ['userID'];
-        $todo->createddate = date("Y/m/d");
-        $todo->isdone = $_POST['isdone'];
-        $todo->message = $_POST['message'];
-        $todo->owneremail = $_POST['owneremail'];
-        $todo->duedate = $_POST['duedate'];
-        $todo->save();
-        header("Location: index.php?page=tasks&action=all");
+        if(key_exists('userID',$_SESSION)) {
+            $userID = $_SESSION['userID'];
+            $record = accounts::findOne($userID);
+            self::getTemplate('new_task', $record);
+        } else {
+            echo 'you must be logged in to create tasks';
+        }
     }
 
     //this is the function to view edit record form
     public static function edit()
     {
-
         $record = todos::findOne($_REQUEST['id']);
-
         self::getTemplate('edit_task', $record);
 
     }
@@ -75,31 +75,32 @@ class tasksController extends http\controller
     //this would be for the post for sending the task edit form
     public static function store()
     {
+        session_start();
+        $userID = $_SESSION['userID'];
+        $userRecord = accounts::findOne($userID);
 
-/*
-        $record = todos::findOne($_REQUEST['id']);
-        $record->body = $_REQUEST['body'];
-        $record->save();
-        print_r($_POST);
-*/
+        $task = new todo();
+
+        $task->message = $_POST['message'];
+        $task->ownerid = $userRecord->id;
+        $task->owneremail = $userRecord->email;
+        $task->createddate = date("Y-m-d");
+        $task->isdone = intval($_POST['isdone']);
+        $task->duedate =  $_POST['duedate'];
+        $task->save();
+        // print_r($_POST);
+        header("Location: index.php?page=tasks&action=all");
+
     }
 
     public static function save() {
-
         $task = todos::findOne($_REQUEST['id']);
+// print_r($_POST);
         $task->message = $_POST['message'];
-        $task->duedate = $_POST['duedate'];
-        $task->owneremail = $_POST['owneremail'];
-        $task->isdone = $_POST['isdone'];
+        $task->isdone = intval($_POST['isdone']);
+        $task->duedate =  $_POST['duedate'];
         $task->save();
-        header("Location: index.php?page=tasks&action=all");
-        //print_r($task);
-       /* session_start();
-        $task = new todo();
-
-        $task->body = $_POST['body'];
-        $task->ownerid = $_SESSION['userID'];
-        $task->save(); */
+        header("Location: index.php?page=tasks&action=show&id=".$_REQUEST['id']);
 
     }
 
@@ -110,6 +111,7 @@ class tasksController extends http\controller
         $record = todos::findOne($_REQUEST['id']);
         $record->delete();
         print_r($_POST);
+        header("Location: index.php?page=tasks&action=all");
 
     }
 
