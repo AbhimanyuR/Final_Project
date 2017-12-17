527ea635bdfc417008908aa8a2713c0e3f220888
Pareekshith A Abhimanyu Raja <paa36@afsconnect2.njit.edu>
2017-12-17 07:20:12 -0500

    Updated show_account layout

--- pages/show_account.php
+++ pages/show_account.php
@@ -1,53 +1,109 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Register page</title>
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="pages/assets/css/style.css?v=1.0">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Mysite</a>
+                </div>
+        
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li  class="active">
+                            <a href="/">MY account</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=tasks&action=create"  >Add New Task</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=tasks&action=all">Tasks</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=accounts&action=logout">Logout</a>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
+        </nav>
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
 
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+                    
+                </div>
+                <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                    <div class="register-form">
+                        <dl class="dl-horizontal">
+                            <dt><h3>Name:</h3></dt>
+                                <dd><h2><?php echo $data->fname." ".$data->lname; ?></h2></dd>
+                            <dt><h3>Email:</h3></dt>
+                                <dd><h2><?php echo $data->email; ?></h2></dd>
+                            <dt><h3>DATE OF BIRTH:</h3></dt>
+                               <dd><h2><?php echo $data->birthday; ?></h2></dd>
+                            <dt><h3>Phone:</h3></dt>
+                                <dd><h2><?php echo $data->phone; ?></h2></dd>
+                            <dt><h3>Gender:</h3></dt>
+                                <dd><h2><?php echo $data->gender; ?></h2></dd>
+                          </dl>
 
-<body>
+                          <a href="index.php?page=accounts&action=edit" class="btn btn-primary">Edit Profile</a>
+                    </div>
+                </div>
 
-<h1>Email: <?php echo $data->email; ?></h1>
-<h1>First Name: <?php echo $data->fname; ?></h1>
-<h1>Last Name: <?php echo $data->lname; ?></h1>
-
-
-<?php
-//this is how you print something  $data contains the record that was selected on the table.
-
-print_r($data);
-
-
-?>
-
-<form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
-
-    First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
-
-    Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>"><br>
-    Email: <input type="text" name="email" value="<?php echo $data->email; ?>"><br>
-    Phone: <input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
-    Birthday: <input type="text" name="birthday" value="<?php echo $data->birthday; ?>"><br>
-    Gender: <input type="text" name="gender" value="<?php echo $data->gender; ?>"><br>
-    <input type="submit" value="Submit form">
-</form>
+                <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                    
+                </div>
+            </div>
+        </div>
 
 
-<form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
-</form>
 
-
-<script src="js/scripts.js"></script>
-</body>
+        <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+            <div class="container">
+                <a class="navbar-brand" href="#">My footer</a>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- jQuery -->
+        <script src="//code.jquery.com/jquery.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+         <script src="Hello World"></script>
+    </body>
 </html>
