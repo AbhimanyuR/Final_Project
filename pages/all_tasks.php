9be52c08cce175373a1637bc787e845c4a194c39
Pareekshith A Abhimanyu Raja <paa36@afsconnect2.njit.edu>
2017-12-17 07:15:08 -0500

    All tasks page layout updated

--- pages/all_tasks.php
+++ pages/all_tasks.php
@@ -1,32 +1,100 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Register page</title>
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
-    <meta name="author" content="SitePoint">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="pages/assets/css/style.css?v=1.0">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <nav class="navbar navbar-default" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Mysite</a>
+                </div>
+        
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse navbar-ex1-collapse">
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="index.php?page=accounts&action=show">MY account</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=tasks&action=create"  >Add New Task</a>
+                        </li>
+                        <li  class="active">
+                            <a href="index.php?page=tasks&action=all">Tasks</a>
+                        </li>
+                        <li>
+                            <a href="index.php?page=accounts&action=logout">Logout</a>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div>
+        </nav>
 
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
 
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
-</head>
-
-<body>
-<a href = "index.php?page=tasks&action=create">Create Task</a>
-
-<?php
-//this is how you print something
-
-print utility\htmlTable::genarateTableFromMultiArray($data);
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="all-task-list">
+                        <?php
+                            //this is how you print something
+                            if ($data) {
+                                print utility\htmlTable::genarateTableFromMultiArray($data);
+                            }else{
+                                echo 'You don\'t have any tasks. Please create one <a href="index.php?page=tasks&action=create" class="btn btn-primary">Add New Task </a>';
+                            }
 
 
-?>
+                        ?>
+
+                          
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
-<script src="js/scripts.js"></script>
-</body>
+
+        <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+            <div class="container">
+                <a class="navbar-brand" href="#">My footer</a>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- jQuery -->
+        <script src="//code.jquery.com/jquery.js"></script>
+        <!-- Bootstrap JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+         <script src="Hello World"></script>
+    </body>
 </html>
